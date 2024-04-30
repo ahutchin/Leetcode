8e68da54cb011bcd58f792cbe436d83d45109479
Alexander Hutchinson <62565003+ahutchin@users.noreply.github.com>
2024-04-30 14:03:02 -0400

    finished Best Time to Buy and Sell stock problem

--- BestTimeToBuyAndSellStock.py
+++ BestTimeToBuyAndSellStock.py
@@ -1,11 +1,13 @@
+from typing import List
+
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit1(self, prices: List[int]) -> int:
         # pointer priceDifference
         priceDifference = 0
 
         # iterate through prices array
         for i in range(len(prices)):
-            for j in range(1, len(prices)):
+            for j in range(i + 1, len(prices)):
                 if prices[j] < prices[i]: # if the difference between the indeces is negative
                     i = j
                     break
@@ -14,3 +16,24 @@ class Solution:
         
         # return
         return priceDifference
+    
+    def maxProfit2(self, prices: List[int]) -> int:
+        # pointer priceDifference
+        buy = prices[0]
+        profit = 0
+
+        for k in range(1, len(prices)):
+            if prices[k] < buy:
+                buy = prices[k]
+            if profit < prices[k] - buy:
+                profit = prices[k] - buy
+        
+        return profit
+    
+# Example test case
+solution = Solution()
+
+prices = [7,6,4,3,1]
+
+result = solution.maxProfit2(prices)
+print(result)
