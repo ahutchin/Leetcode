6235237a660f1a1d89c5ec672a0201c85ae77f6c
Alexander Hutchinson <ahutchin2003@gmail.com>
2024-04-30 15:24:23 -0400

    solved Majority element

---
+++ MajorityElement.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # initialize majority element
+        majorityElement = nums[0]
+        count = 1
+
+        # iterate through the array
+        for i in range(1, len(nums)):
+            if nums[i] == majorityElement:
+                count += 1
+            else: 
+                count -= 1
+                if count == -1:
+                    majorityElement = nums[i]
+                    count = 1
+        
+        # return majorityElement
+        return majorityElement
