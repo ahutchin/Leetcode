7ca2c0fea5d6bd2cbebc54bc884e6d5100cedcf1
Alexander Hutchinson <ahutchin2003@gmail.com>
2024-05-06 14:57:13 -0400

    Solved Valid Palindrome

--- ValidPalindrome.py
+++ ValidPalindrome.py
@@ -1,3 +1,36 @@
 class Solution:
+    # Space Complexity: O(1)
+    # Time Complexity: O(n)
     def isPalindrome(self, s: str) -> bool:
-        # strip the string of capitals and non letters
+        # Initialize Alphabet
+        alphabet = "abcdefghijklmnopqrstuvwxyz1234567890"
+
+        # Set to lowercase
+        s = s.lower()
+
+        # 2 pointers
+        i, j = 0, len(s) - 1
+
+        # Iterate from either end
+        while i <= j:
+            if s[i] not in alphabet or s[j] not in alphabet:
+                if s[i] not in alphabet:
+                    i += 1
+                if s[j] not in alphabet:
+                    j -= 1
+            elif s[i] == s[j]:
+                i += 1
+                j -= 1
+            elif s[i] != s[j]:
+                return False
+        
+        # Return
+        return True
+    
+# Test Case
+solution = Solution()
+
+s = "0P"
+
+result = solution.isPalindrome(s)
+print(result)
