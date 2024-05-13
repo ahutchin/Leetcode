b9e85b1a4b3125ff12c25b0672729505971df73e
Alexander Hutchinson <62565003+ahutchin@users.noreply.github.com>
2024-05-13 18:22:15 -0400

    Solved Balanced Binary Tree

---
+++ BalancedBinaryTree.py
@@ -0,0 +1,27 @@
+from typing import Optional, Tuple
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    # Time Complexity: 0(n) because each tree node is only visited once
+    # Space Complexity: O(1) because no additional memory is being used
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+
+        def dfs(root: Optional[TreeNode]):
+            # Base Case
+            if not root: return [True, 0]
+
+            # Recursively call dfs on left and right node
+            left, right = dfs(root.left), dfs(root.right)
+            balanced = (left[0] and right[0]) and (abs(left[1] - right[1]) <= 1)
+
+            # Return
+            return [balanced, 1 + max(left[1], right[1])]
+        
+        return dfs(root)[0]
