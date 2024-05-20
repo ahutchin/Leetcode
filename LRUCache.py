1e19595980faf21bbb18e5839dd70188c1a5f4c0
Alexander Hutchinson <ahutchin2003@gmail.com>
2024-05-20 18:02:30 -0400

    Started LRU Cache

---
+++ LRUCache.py
@@ -0,0 +1,18 @@
+https://www.youtube.com/watch?v=7ABFKPK2hD4&ab_channel=NeetCode
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        
+
+    def get(self, key: int) -> int:
+        
+
+    def put(self, key: int, value: int) -> None:
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
