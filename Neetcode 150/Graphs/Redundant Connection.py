9584e299adcfc4c4c1a0e9f9cf9f5d0ac1e51231
Alexander Hutchinson <ahutchin2003@gmail.com>
2025-11-25 16:28:56 -0500

    Solved Redundant Conection

--- Neetcode 150/Graphs/Redundant Connection.py
+++ Neetcode 150/Graphs/Redundant Connection.py
@@ -15,7 +15,22 @@ class Solution:
     #         visited.add(neighbor)
 
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        parent = [i for i in range(len(edges) + 1)]
+        def find(node) -> int:
+            if parent[node] == node:
+                return node
+            parent[node] = find(parent[node])
+            return parent[node]
         
+        def union(node, neighbor) -> None:
+            root1 = find(node)
+            root2 = find(neighbor)
+            parent[root2] = root1
+
+        for node, neighbor in edges:
+            if find(node) == find(neighbor):
+                return [node, neighbor]
+            union(node, neighbor)
 
 
 # Intuition:
@@ -26,3 +41,8 @@ class Solution:
 # Everytime we add an edge, we check if theres a loop
 
 # O(E) -> Add each neigbor, node pair to graph, if the node is alr in seen, then its invalid
+
+print(Solution().findRedundantConnection([[1,4],[3,4],[1,3],[1,2],[4,5]]))
+
+# Time Complexity: O(E) - for every edge we do an O(1) operation (find, then union)
+# Space Complexity: O(V) - we store every vertex and its parent
